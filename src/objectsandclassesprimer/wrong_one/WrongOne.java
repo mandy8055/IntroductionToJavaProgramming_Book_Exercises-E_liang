fd08071cbcbf4b761593d56e200007023ab7d833
Mandy8055 <msankrit8055@gmail.com>
2020-02-23 14:08:30 +0530

    Added GUIComponent class and WrongOne package

---
+++ src/objectsandclassesprimer/wrong_one/WrongOne.java
@@ -0,0 +1,10 @@
+package objectsandclassesprimer.wrong_one;
+
+public class WrongOne {
+    public static void main(String[] args){
+        int x; // x has no default value
+        String y; // y has no default value
+//        System.out.println("x is " + x); // Error: x might not have been initialized
+//        System.out.println("y is " + y); // Error: x might not have been initialized
+    }
+}
