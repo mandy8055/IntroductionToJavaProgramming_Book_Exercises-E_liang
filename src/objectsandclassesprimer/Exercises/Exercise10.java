d20f070906ba46a65c805f2d20e8c100a1a2e1d4
Mandy8055 <msankrit8055@gmail.com>
2020-03-06 21:47:47 +0530

    Added Exercise solutions for problems 8, 9 and 10

---
+++ src/objectsandclassesprimer/Exercises/Exercise10.java
@@ -0,0 +1,46 @@
+package objectsandclassesprimer.Exercises;
+import java.util.Scanner;
+
+public class Exercise10 {
+
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args){
+
+        System.out.print("Enter a, b, c: ");
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+        QuadraticEquation quadraticEquation = new QuadraticEquation(a, b, c);
+        if(quadraticEquation.getDiscriminant() == 0)System.out.println("The root is " + quadraticEquation.getRoot1());
+        else if(quadraticEquation.getDiscriminant() < 0)System.out.println("The equation has no real roots");
+        else System.out.println("The roots are " + quadraticEquation.getRoot1() + " and " + quadraticEquation.getRoot2());
+        sc.close();
+    }
+}
+
+class QuadraticEquation{
+    private double a, b, c;
+    
+    public QuadraticEquation(double aVal, double bVal, double cVal){
+        a = aVal;
+        b = bVal;
+        c = cVal;
+    }
+    
+    public double getA(){return a;}
+    public double getB(){return b;}
+    public double getC(){return c;}
+    
+    public double getDiscriminant(){
+        return ((b * b) - (4 * a * c));
+    }
+
+    public double getRoot1(){
+        return ((-b) + Math.sqrt(getDiscriminant())) / 2 * a;
+    }
+
+    public double getRoot2(){
+        return ((-b) - Math.sqrt(getDiscriminant())) / 2 * a;
+    }
+}
